00e4e3ebc19ae038f7c8c393b23d4234e9cf666e
rluria14 <ryanlur14@gmail.com>
2021-04-22 17:04:21 -0400

    try removing cp runsc line

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 bazel build --test_output=errors --keep_going --verbose_failures=true //runsc:runsc
-mkdir ${PREFIX}/bin
-cp ./bazel-out/k8-fastbuild-ST-4c64f0b3d5c7/bin/runsc/runsc_/runsc ${PREFIX}/bin
