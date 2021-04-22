48215ab8b2f4f5cd4131bb5b1cd4b4530e87baa4
rluria14 <ryanlur14@gmail.com>
2021-04-22 16:56:39 -0400

    try new location

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,4 @@
 
 bazel build --test_output=errors --keep_going --verbose_failures=true //runsc:runsc
 mkdir ${PREFIX}/bin
-ls -a ./bazel-bin
-ls -a ./bazel-bin/runsc
-cp ./bazel-bin/runsc/linux_amd64_pure_stripped/runsc ${PREFIX}/bin
+cp ./bazel-out/k8-fastbuild-ST-4c64f0b3d5c7/bin/runsc/runsc_/runsc ${PREFIX}/bin
