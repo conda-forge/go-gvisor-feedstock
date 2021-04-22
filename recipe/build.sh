6885f2ff4c202f1a02a98f7e3d4121085f9ab1df
rluria14 <ryanlur14@gmail.com>
2021-04-22 16:40:38 -0400

    list more

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,6 @@
 
 bazel build --test_output=errors --keep_going --verbose_failures=true //runsc:runsc
 mkdir ${PREFIX}/bin
-ls -a
+ls -a ./bazel-bin
+ls -a ./bazel-bin/runsc
 cp ./bazel-bin/runsc/linux_amd64_pure_stripped/runsc ${PREFIX}/bin
