7e428ae198d5f42491b4b29c92d8b593248cfea8
rluria14 <ryanlur14@gmail.com>
2021-04-22 16:15:52 -0400

    find runsc

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 bazel build --test_output=errors --keep_going --verbose_failures=true //runsc:runsc
 mkdir ${PREFIX}/bin
-ls -a
+find . -name "*runsc*"
 cp ./bazel-bin/runsc/linux_amd64_pure_stripped/runsc ${PREFIX}/bin
